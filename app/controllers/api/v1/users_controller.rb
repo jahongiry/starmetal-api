b77fc38a48445880413d0092ee3ade44f07f0ca9
Jahongir Yusupov <91022355+jahongiry@users.noreply.github.com>
2024-04-03 16:42:00 +0500

    Add devise user

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,22 @@
+# app/controllers/api/v1/users_controller.rb
+module Api
+  module V1
+    class UsersController < ApplicationController
+      def create
+        @user = User.new(user_params)
+
+        if @user.save
+          render json: @user, status: :created
+        else
+          render json: @user.errors, status: :unprocessable_entity
+        end
+      end
+
+      private
+
+      def user_params
+        params.require(:user).permit(:email, :phone_number, :password)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
+# config/routes.rb
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create]
+      post '/login', to: 'sessions#create'
+    end
+  end
 end
